e71540a7296fad3b5e06a281746586a1d98c0825
suthiwat <joestw1996@gmail.com>
2021-04-20 20:37:06 +0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Covid-19 Info
         </a>
       </header>
     </div>
