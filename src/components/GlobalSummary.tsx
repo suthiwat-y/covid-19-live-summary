dca545c05bab7c19ddcb988a252cb67813cffb30
suthiwat <joestw1996@gmail.com>
2021-04-22 22:33:56 +0700

    Make responsive with mobile browser

--- src/components/GlobalSummary.tsx
+++ src/components/GlobalSummary.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Col, Row, Statistic } from "antd";
+import { Card, Col, Row, Typography } from "antd";
+import { renderNumber } from "../utils/renderNumber";
 
 type GlobalData = {
   TotalConfirmed: number;
@@ -14,60 +15,65 @@ interface GlobalSummaryProps {
 const GlobalSummary: React.FC<GlobalSummaryProps> = ({ globalData }) => {
   return (
     <Row
+      gutter={[16, 24]}
       style={{
         justifyContent: "space-evenly",
         paddingTop: 24,
         paddingBottom: 24,
       }}
     >
-      <Col style={{ width: "24%" }}>
-        <Statistic
+      <Col>
+        <Card
           title="Total Confirmed Cases"
-          value={
-            globalData.TotalConfirmed ? globalData.TotalConfirmed : "unreported"
-          }
-          valueStyle={{ fontWeight: "bold" }}
           style={{
             backgroundColor: "#222",
-            padding: 24,
+            padding: 18,
             textAlign: "center",
-            textSizeAdjust: "auto",
+            fontSize: "1.5rem",
           }}
-        />
+        >
+          <Typography.Text strong={true} ellipsis={true}>
+            {renderNumber(globalData.TotalConfirmed)}
+          </Typography.Text>
+        </Card>
       </Col>
-      <Col style={{ width: "24%" }}>
-        <Statistic
+      <Col>
+        <Card
           title="Total Deaths"
-          value={globalData.TotalDeaths ? globalData.TotalDeaths : "unreported"}
-          valueStyle={{
-            color: "#dd0000",
-            fontWeight: "bold",
-          }}
           style={{
             backgroundColor: "#222",
-            padding: 24,
+            padding: 18,
             textAlign: "center",
-            textSizeAdjust: "auto",
+            fontSize: "1.5rem",
           }}
-        />
+        >
+          <Typography.Text
+            strong={true}
+            ellipsis={true}
+            style={{ color: "#dd0000" }}
+          >
+            {renderNumber(globalData.TotalDeaths)}
+          </Typography.Text>
+        </Card>
       </Col>
-      <Col style={{ width: "24%" }}>
-        <Statistic
+      <Col>
+        <Card
           title="Total Recovered"
-          value={
-            globalData.TotalRecovered ? globalData.TotalRecovered : "unreported"
-          }
-          valueStyle={{
-            color: "#00dd00",
-            fontWeight: "bold",
-          }}
           style={{
             backgroundColor: "#222",
-            padding: 24,
+            padding: 18,
             textAlign: "center",
-            textSizeAdjust: "auto",
+            fontSize: "1.5rem",
           }}
-        />
+        >
+          <Typography.Text
+            strong={true}
+            ellipsis={true}
+            style={{ color: "#00dd00" }}
+          >
+            {renderNumber(globalData.TotalRecovered)}
+          </Typography.Text>
+        </Card>
       </Col>
     </Row>
   );
